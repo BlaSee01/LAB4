f8c6acc307a16c1347efe0dc64c8f36dc6ea41ca
BlaSee01 <92850744+BlaSee01@users.noreply.github.com>
2021-12-01 16:45:05 +0100

    ZAD5

---
+++ ZAD5/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int funkcja(int a){
+    if(a>=0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int main()
+{
+
+    funkcja(9);
+
+    return 0;
+}
